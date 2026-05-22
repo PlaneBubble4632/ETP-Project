acd244f406ec203ddc5a88089abf710a1ef7bc54
PlaneBubble4632 <leonardedgartjandra@gmail.com>
2026-05-22 20:30:12 +0700

    #

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 window = tk.Tk()
 window.title("ETP game")
 
-lbl = tk.Label(root, text="Hello World")
+lbl = tk.Label(window, text="Hello World")
 lbl.pack()
 
 window.mainloop()
