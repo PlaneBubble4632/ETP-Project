e2015a6802daa7855c29d05ecc70f229b7c14799
PlaneBubble4632 <leonardedgartjandra@gmail.com>
2026-05-22 22:04:32 +0700

    #

--- ETP Project/main.py
+++ ETP Project/main.py
@@ -1,8 +1,56 @@
 import tkinter as tk
 window = tk.Tk()
-window.geometry()
+window.geometry("900x600")
 window.title("ETP game")
 
+title = tk.Label(window, text="Entreprenuership Game Simulator", font=("Arial", 30))
+title.pack(pady=20)
+
+money = 100
+money_label = tk.Label(window, text=f"Money: ${money}", font=("Arial", 30))
+money_label.pack()
+
+reputation = 50
+reputation_label = tk.Label(window, text=f"Reputation: {reputation}", font=("Arial", 30))
+reputation_label.pack()
+
+customer_satisfaction = 10
+customer_satisfaction_label = tk.Label(window, text=f"Customer Satisfaction: {customer_satisfaction}", font=("Arial", 30))
+customer_satisfaction_label.pack()
+
+scenario = tk.Label(window, text="Scenario: Customer complains the drink is too expensive", font=("Arial", 20))
+scenario.pack(pady=35)
+
+def lower_price():
+    global money
+    money -= 10
+    global customer_satisfaction
+    customer_satisfaction += 5
+    customer_satisfaction_label.config(text=f"Customer Satisfaction: {customer_satisfaction}")
+    money_label.config(text=f"Money: ${money}")
+    scenario.config(text="You lowered the price. Customers are happy but your profit is reduced.")
+
+def improve_quality():
+    global money
+    money -= 20
+    global reputation
+    reputation += 10
+    money.config(text=f"Money: ${money}")
+    scenario.config(text="You improved the quality. Customers are happy and your reputation improves.")
+
+def ignore():
+    scenario.config(text="You ignored the complaint. Customers are dissatisfied.")
+
+button1 = tk.Button(window, text="Lower Price", font=("Arial", 20), command=lower_price, width=15)
+button1.pack(pady=20)
+
+button2 = tk.Button(window, text="Improve Quality", font=("Arial", 20), command=improve_quality, width=15)
+button2.pack(pady=20)
+
+button3 = tk.Button(window, text="Ignore", font=("Arial", 20), command=ignore, width=15)
+button3.pack(pady=20)
 
 
+
+window.configure(bg="lightblue")
 window.mainloop()
